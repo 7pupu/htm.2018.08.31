ea8b0892d4ae1db7710ef10749d73ff4c4b184ff
alice <wotupset@gmail.com>
2018-08-31 22:48:04 +0800

    Add files via upload

---
+++ demo_worker.js
@@ -0,0 +1,15 @@
+﻿var i = 0;
+
+function timedCount() {
+    i = i + 1;
+    postMessage([i,"ㄏㄏ"]);//傳回主視窗
+    setTimeout("timedCount()",500);
+}
+//timedCount();
+self.onmessage = function(event){};
+self.addEventListener("message", function(event){
+	//console.log(event); //印不出來
+	self.postMessage(["傳給主視窗",event.data]);
+}, false);//監聽message事件
+
+//self.close();//結束worker
